aae73a52463c9a4a5cc3390b315b150cf23f0cef
DESKTOP-NSP0TBN\user1 <zoshsthfwj@naver.com>
2026-07-20 17:31:28 +0900

    0720 수업내용

---
+++ python/prj03/test02.py
@@ -0,0 +1,13 @@
+# 변수
+
+x,y="10",20
+print(x)
+print(y)
+
+# 동적 타이핑
+
+print(type(x))
+print(type(int(x)))
+
+del(x)
+print(x)
---
+++ python/prj03/test03.py
@@ -0,0 +1,5 @@
+# 연산자
+
+age=int(input("How old are you?"))
+is_adult=age>=20
+print(f"성인 여부 : {is_adult}")
