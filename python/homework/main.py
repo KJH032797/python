7fa9c1ab4c9d2d1b2dd690b1629d8bdf784a17b8
DESKTOP-J1LF37O\User <zoshsthfwj@naver.com>
2026-07-25 12:39:37 +0900

    no message

--- python/homework/main.py
+++ python/homework/main.py
@@ -1,16 +1,35 @@
-# 샘플 Python 스크립트입니다.
+# 자판기
 
-# Shift+F10을(를) 눌러 실행하거나 내 코드로 바꿉니다.
-# 클래스, 파일, 도구 창, 액션 및 설정을 어디서나 검색하려면 Shift 두 번을(를) 누릅니다.
-
-
-def print_hi(name):
-    # 스크립트를 디버그하려면 하단 코드 줄의 중단점을 사용합니다.
-    print(f'Hi, {name}')  # 중단점을 전환하려면 Ctrl+F8을(를) 누릅니다.
+menu = {"콜라" : 1500, "사이다" : 1300, "커피" : 2000}
+print(menu)
+money = int(input("돈을 넣어주세요: "))
 
 
-# 스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+if money >= 1300 :
+    while True :
+        print(f"\n현재 잔액 {money}원")
+        order = input("메뉴를 선택해주세요(종료를 원하면 '종료' 입력): ")
+        if order in menu :
+            if money >= menu[order]:
+                money -= menu[order]
+                print(f"{menu[order]}원 차감되었습니다.")
 
-# https://www.jetbrains.com/help/pycharm/에서 PyCharm 도움말 참조
+                if money < 1300:
+                    print("\n잔액이 부족합니다. 이용해주셔서 감사합니다.")
+                    print(f"거스름돈 : {money}원.")
+                    break
+
+            else :
+                print("잔액이 부족합니다.")
+
+        elif order == "종료" :
+            print("\n이용해주셔서 감사합니다.")
+            print(f"거스름돈 : {money}원.")
+            break
+
+        else :
+            print("목록에 없는 메뉴입니다. 다시 입력해주세요.")
+
+else :
+    print("\n잔액이 부족합니다. 이용해주셔서 감사합니다.")
+    print(f"거스름돈 : {money}원.")
